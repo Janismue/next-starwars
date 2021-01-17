af180b173d49361960f491ec36d222db0676e606
Janis Müller <janis_mueller@gmx.de>
2021-01-17 16:18:16 +0100

    First version

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-2xl text-center my-6 font-semibold">
+                Welcome to Next.js Starwars Website
+            </h1>
+            <p className="mb-5">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+            <p className="mb-5">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+            <p className="mb-5">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+        </div>
+    );
+}
---
+++ pages/planets/index.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import React from 'react';
+
+export async function getStaticProps(context) {
+    const res = await fetch('https://swapi.dev/api/planets');
+    const data = await res.json();
+
+    if (!data) {
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: { data },
+    };
+}
+
+export default function Planets({ data }) {
+    return (
+        <div>
+            <h1 className="text-center text-xl font-semibold my-6">Planets</h1>
+            <div className="flex flex-wrap justify-center">
+                {data.results.map((planet) => {
+                    const urlArr = planet.url.split('/');
+                    const id = urlArr[urlArr.length - 2];
+
+                    return (
+                        <div
+                            key={id}
+                            className="mx-2 mb-2 bg-gray-700 py-2 px-4 rounded-md text-white"
+                        >
+                            <Link href={`/planets/${id}`}>{planet.name}</Link>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
